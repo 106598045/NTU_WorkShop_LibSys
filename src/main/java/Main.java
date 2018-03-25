b86dc5042a3b74983f1d24f7969dfb272107f42a
hanlong <t106598045@ntut.edu.tw>
2018-03-25 16:49:46 +0800

    input paser 改用 queue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,16 @@ import bean.Book;
 import model.InputHandler;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by gunch on 2018/3/24.
  */
 public class Main {
-    static int currentIndex = 0;
-    static List<String> commandList = new ArrayList<String>();
+    static Queue<String> commandList = new LinkedList<String>();
+    //static List<String> commandList = new ArrayList<String>();
     public static void main(String[] args) {
         String path = "";
         path = ""+ InputHandler.class.getClassLoader().getResource("");
@@ -18,62 +20,72 @@ public class Main {
         String content = inputHandler.getInputByFile();
 
         final String[] splittedStr = content.split("\n");
-        commandList = new ArrayList<String>();
         for (String s : splittedStr) {
             commandList.add(s);
+            //System.out.println(commandList.poll());
         }
+
+
        // System.out.println(commandList);
         getBookByCommand();
-        System.out.println(currentIndex);
         getUserByCommand();
-        System.out.println(currentIndex);
+
     }
 
     public static void getBookByCommand(){
-        for(int i= currentIndex;i<commandList.size();i++){
-            if(commandList.get(currentIndex).matches("\\d")){
-                //System.out.println(commandList.get(i));
-                int bookNum = Integer.parseInt(commandList.get(i));
+        for(int i= 0;i<commandList.size();i++){
+            String currentCmd = commandList.poll();
+            if(currentCmd.matches("\\d")){
+                int bookNum = Integer.parseInt(currentCmd);
                 for(int j=0;j<bookNum;j++){
-                    String[] s = commandList.get(currentIndex).split("");
+                    String[] s = commandList.poll().split(" ");
                     String bookAuthor = s[0];
                     String bookSubject = s[1];
-                    currentIndex++;
+                    System.out.println(bookAuthor+","+bookSubject);
                 }
-                currentIndex = currentIndex+1;
                 return;
             }
-            currentIndex++;
         }
     }
 
     public static void getUserByCommand(){
-        for(int i= currentIndex;i<commandList.size();i++){
-            if(commandList.get(currentIndex).matches("\\d")){
-                //System.out.println(commandList.get(i));
-                int UserNum = Integer.parseInt(commandList.get(i));
-                for(int j=0;j<UserNum;j++){
-                    //建User
+        for(int i= 0;i<commandList.size();i++){
+            String currentCmd = commandList.poll();
+            if(currentCmd.matches("\\d")){
+                int userNum = Integer.parseInt(currentCmd);
+                for(int j=0;j<userNum;j++){
+                    String[] s = commandList.poll().split(" ");
+                    String userType = s[0];
+                    String userName = s[1];
+                    System.out.println(userType+","+userName);
                 }
-                currentIndex = i+UserNum+1;
                 return;
             }
-            currentIndex++;
         }
     }
 
     public static void getBehaviorByCommand(){
-        for(int i= currentIndex;i<commandList.size();i++){
-            if(commandList.get(currentIndex).matches("\\d")){
-                //System.out.println(commandList.get(i));
-                int UserNum = Integer.parseInt(commandList.get(i));
-                for(int j=0;j<UserNum;j++){
-                    //建User
-                }
-                currentIndex = i+UserNum+1;
-                return;
+        while (!commandList.isEmpty()){
+            String currentCmd = commandList.poll();
+            if(currentCmd.contains("addBook")){
+
+            }else if(currentCmd.contains("addBook")){
+
+            }else if(currentCmd.contains("removeBook")){
+
+            }else if(currentCmd.contains("checkout")){
+
+            }else if(currentCmd.contains("return")){
+
+            }else if(currentCmd.contains("listAuthor")){
+
+            }else if(currentCmd.contains("listSubject")){
+
+            }else if(currentCmd.contains("listSubject")){
+
+            }else if(currentCmd.contains("findBorrower")){
+
             }
-            currentIndex++;
         }
     }
 }
